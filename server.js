6d9496662f243f1fbc093b3fc4ba38664c78349e
mmd amin ahmdi <en.mmdamin@gmail.com>
2021-09-03 19:13:42 +0430

    server initilized

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("<h1>hello</h1>")
+})
+app.listen(3000,()=>console.log("server running on port 3000"))
